0a6f4074e058a331f7eec94e76e1895e1e445e4f
Switchbreak <switch@switchbreak.net>
2022-11-11 15:11:48 -0800

    Add touch controls for camera

--- Assets/OrbitCamera.cs
+++ Assets/OrbitCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class OrbitCamera : MonoBehaviour
 {
-    private const int LEFT_MOUSE_BUTTON = 0;
+    private const int RIGHT_MOUSE_BUTTON = 1;
     private const int MIDDLE_MOUSE_BUTTON = 2;
     private const int DRAG_CLAMP = 100;
 
@@ -13,18 +13,26 @@ public class OrbitCamera : MonoBehaviour
     Vector3 lookAt = default;
 
     [SerializeField]
-    float orbitSpeed = 50;
+    float orbitMouseSpeed = 50;
 
     [SerializeField]
-    float zoomSpeed = 1;
+    float orbitTouchSpeed = 10;
 
     [SerializeField]
-    float panSpeed = 0.01f;
+    float zoomWheelSpeed = 1;
+
+    [SerializeField]
+    float zoomTouchSpeed = 0.0001f;
+
+    [SerializeField]
+    float panMouseSpeed = 0.01f;
+
+    [SerializeField]
+    float panTouchSpeed = 0.01f;
 
     [SerializeField]
     public bool pieceDragging = false;
 
-    Vector3 up = new(0, 1, 0);
     Vector3 mousePosition;
 
     // Start is called before the first frame update
@@ -32,37 +40,80 @@ public class OrbitCamera : MonoBehaviour
     {
         this.transform.LookAt(lookAt);
         mousePosition = Input.mousePosition;
+
+        Input.simulateMouseWithTouches = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(LEFT_MOUSE_BUTTON) && !pieceDragging)
+        MouseInput();
+        TouchInput();
+    }
+
+    private void TouchInput()
+    {
+        // Orbit camera on single touch
+        if (Input.touchCount == 1 && !pieceDragging)
         {
-            var drag = Input.mousePosition - mousePosition;
-
-            var dragX = Mathf.Clamp(drag.x, -DRAG_CLAMP, DRAG_CLAMP);
-            var dragY = -Mathf.Clamp(drag.y, -DRAG_CLAMP, DRAG_CLAMP);
-
-            this.transform.RotateAround(lookAt, up, dragX * orbitSpeed * Time.deltaTime);
-            this.transform.RotateAround(lookAt, this.transform.right, dragY * orbitSpeed * Time.deltaTime);
-            this.transform.LookAt(lookAt);
+            Orbit(Input.touches[0].deltaPosition, orbitTouchSpeed);
         }
 
+        if (Input.touchCount == 2)
+        {
+            var moveMagnitude1 = Vector2.Dot(Input.touches[0].deltaPosition, Input.touches[1].position - Input.touches[0].position);
+            var moveMagnitude2 = Vector2.Dot(Input.touches[1].deltaPosition, Input.touches[0].position - Input.touches[1].position);
+
+            if (Mathf.Sign(moveMagnitude1) == Mathf.Sign(moveMagnitude2))
+            {
+                this.GetComponent<Camera>().fieldOfView += zoomTouchSpeed * (moveMagnitude1 + moveMagnitude2);
+            }
+
+            var panMagnitude = Vector2.Dot(Input.touches[0].deltaPosition, Input.touches[1].deltaPosition);
+            if (panMagnitude > 0)
+            {
+                var averageDragVector = (Input.touches[0].deltaPosition + Input.touches[1].deltaPosition) / 2;
+                Pan(averageDragVector, panTouchSpeed);
+            }
+        }
+    }
+
+    private void MouseInput()
+    {
+        if (Input.GetMouseButtonDown(RIGHT_MOUSE_BUTTON))
+        {
+            mousePosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(RIGHT_MOUSE_BUTTON))
+        {
+            Orbit(Input.mousePosition - mousePosition, orbitMouseSpeed);
+        }
         if (Input.GetMouseButton(MIDDLE_MOUSE_BUTTON))
         {
-            var drag = Input.mousePosition - mousePosition;
-
-            var panOffset = this.transform.position;
-            this.transform.transform.Translate(-drag * panSpeed, Space.Self);
-            lookAt += (this.transform.position - panOffset);
+            Pan(Input.mousePosition - mousePosition, panMouseSpeed);
         }
-
         if (Input.mouseScrollDelta.y != 0)
         {
-            this.GetComponent<Camera>().fieldOfView -= zoomSpeed * Input.mouseScrollDelta.y;
+            this.GetComponent<Camera>().fieldOfView -= zoomWheelSpeed * Input.mouseScrollDelta.y;
         }
 
         mousePosition = Input.mousePosition;
+    }
+
+    private void Pan(Vector3 drag, float panSpeed)
+    {
+        var panOffset = this.transform.position;
+        this.transform.transform.Translate(-drag * panSpeed, Space.Self);
+        lookAt += (this.transform.position - panOffset);
+    }
+
+    private void Orbit(Vector3 drag, float orbitSpeed)
+    {
+        var dragX = Mathf.Clamp(drag.x, -DRAG_CLAMP, DRAG_CLAMP);
+        var dragY = -Mathf.Clamp(drag.y, -DRAG_CLAMP, DRAG_CLAMP);
+
+        this.transform.RotateAround(lookAt, Vector3.up, dragX * orbitSpeed);
+        this.transform.RotateAround(lookAt, this.transform.right, dragY * orbitSpeed);
+        this.transform.LookAt(lookAt);
     }
 }
