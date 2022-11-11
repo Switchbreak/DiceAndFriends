c80c406b4fa6c0db380d4bb37b60eb71b9e322ac
Switchbreak <switch@switchbreak.net>
2022-11-10 17:01:54 -0800

    Create collision plane when dragging pieces

--- Assets/ChessPiece.cs
+++ Assets/ChessPiece.cs
@@ -5,31 +5,35 @@ using UnityEngine;
 public class ChessPiece : MonoBehaviour
 {
     private const int MAX_RAYCAST_DISTANCE = 1000;
+    private const int COLLISION_PLANE_SIZE = 5;
+    private const int COLLISION_PLANE_LAYER = 3;
 
     [SerializeField]
     float speed = 0.1f;
 
-    int layerMask;
+    GameObject collisionPlane;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnMouseDown()
     {
-        layerMask = LayerMask.GetMask("Collision Plane");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        collisionPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        collisionPlane.transform.localScale = new Vector3(COLLISION_PLANE_SIZE, COLLISION_PLANE_SIZE, COLLISION_PLANE_SIZE);
+        collisionPlane.GetComponent<MeshRenderer>().enabled = false;
+        collisionPlane.layer = COLLISION_PLANE_LAYER;
     }
 
     private void OnMouseDrag()
     {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         
-        if (Physics.Raycast(ray, out var hitInfo, MAX_RAYCAST_DISTANCE, layerMask))
+        if (Physics.Raycast(ray, out var hitInfo, MAX_RAYCAST_DISTANCE, 1 << COLLISION_PLANE_LAYER))
         {
             var drag = hitInfo.point - this.transform.position;
             this.transform.Translate(drag.x, 0, drag.z, Space.World);
         }
+    }
+
+    private void OnMouseUp()
+    {
+        Destroy(collisionPlane);
     }
 }
